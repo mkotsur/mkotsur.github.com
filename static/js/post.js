564123a72ca63ae56501006795c5174e67cc90ce
Mike Kotsur <miccots@gmail.com>
2016-11-06 09:07:39 +0100

    Removed sharing

--- static/js/post.js
+++ static/js/post.js
@@ -7,7 +7,6 @@
  */
 $(document).ready(function() {
     generateContent();
-    share();
     disqus();
 });
 
@@ -20,11 +19,6 @@ function generateContent() {
     $mt.each(function(i,o){
         $(o).html(toc);
     });
-}
-
-function share(){
-    window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdMiniList":false,"bdPic":"","bdStyle":"1","bdSize":"24"},"share":{}};
-    with(document)0[getElementsByTagName("script")[0].parentNode.appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];
 }
 
 
